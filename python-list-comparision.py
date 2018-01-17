ae4e7aee180f40ed10469eb7515cc04b20a27d7b
irtiza <iali@an10.io>
2018-01-17 15:59:58 +0500

    code updated

--- python-list-comparision.py
+++ python-list-comparision.py
@@ -6,10 +6,14 @@
 # LastUpdated: 12/01/2018
 ####################################################################################################
 
+import copy
+
 def compare_list(list1, list2):
     '''
     It will compare two python lists and return boolean result
     '''
+    lis1 = copy.deepcopy(list1)
+    lis2 = copy.deepcopy(list2)
     if len(lis2) != len(lis1):
         return False
     for val in lis2:
