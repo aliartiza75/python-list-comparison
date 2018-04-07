e5b7dfebd9b7d10ed684572a3dc8e23e84c61978
irtiza <iali@an10.io>
2018-04-08 00:31:42 +0500

    renamed the test file and moved it to the pylistcompare folder

---
+++ pylistcompare/test_pylistcompare.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+###################################################################################################
+# Name: test_pylistcompare.py
+# Summary: File contains tests for the pylistcompare file
+# Author(s): Irtiza Ali
+# LastUpdated: 08/04/2018
+####################################################################################################
+import pylistcompare as plc
+
+# Tests
+lis1 = [1,2,3]
+lis2 = [2,1]
+
+print(plc.compare_list(lis1, lis2))
+
+lis1 = [1,2,3]
+lis2 = [2,1,"3"]
+
+print(plc.compare_list(lis1, lis2))
+
+lis1 = ['1','2','3']
+lis2 = ['2','1']
+
+print(plc.compare_list(lis1, lis2))
+
+
+lis1 = [1,2,3]
+lis2 = [2,1,3]
+
+print(plc.compare_list(lis1, lis2))
+
+lis1 = [{1:1,2:2}]
+lis2 = [{1:1,2:2}]
+
+print(plc.compare_list(lis1, lis2))
+
