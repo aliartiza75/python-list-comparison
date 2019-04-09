5191d14cf0d193bfa0bb7451c5c78b01cc785c75
irti <aliartiza75@yahoo.com>
2019-04-09 14:01:48 +0500

    [master] added requirements.txt file and updated README.md and setup.py file

--- README.md
+++ README.md
@@ -1,10 +1,69 @@
 # pylistcompare
-Code to compare two python list and return the result as a boolean
 
-# To cythonize the code
-cd pythonlistcompare && python setup.py build_ext --inplace 
+A module to compare two lists that complex data in it.
 
 
-# Pypi module page
+# Installation guidelines
+
+* Install the module
+```bash
+$ pip3 install listcompare
+```
+OR
+
+```bash
+$ pip install listcompare
+```
+
+## Usage guidelines
+
+Import the module in your file or ipython3 shell
+
+```bash
+$ import listcompare
+
+$ lis1 = [1, 2, 3]
+$ lis2 = [2, 1, 3]
+
+$ listcompare.compare_list(lis1, lis2)
+$[output] True
+
+
+$ lis1 = [1, 2, "3"]
+$ lis2 = [2, 1, 3]
+
+$ listcompare.compare_list(lis1, lis2)
+$[output] False
+
+``` 
+
+## Project dependencies installation guidelines
+
+Run this command to install the dependencies
+```bash
+$ sudo pip3 install -r requirements.txt
+```
+
+## Project distribution pusblishing guidelines
+
+* Update the setup.py file in the root directory.
+* To create source distribution run the command given below:
+```bash
+$ python3 setup.py sdist
+```
+* I will use [twine](https://pypi.org/project/twine/) to publish my package distribution
+```bash
+$ pip3 install twine
+$ sudo twine upload dist/* # it will ask for username and password
+```
+Sometime twine can publish the distribution because of authentication issue but mostly works on 2nd or 3rd retry.
+
+## Guidelines for cythonizing the project
+```bash
+$ cd pythonlistcompare
+$ python setup.py build_ext --inplace 
+```
+
+# Project distribution page of pypi
 
 Link to the pypi module [page](https://pypi.org/project/listcompare/).
--- setup.py
+++ setup.py
@@ -1,39 +1,42 @@
 from setuptools import setup
 
 setup(name='listcompare',
-      version='1.1.10',
+      version='2.1.1',
       url='https://github.com/aliartiza75/python-list-comparison.git',
       author='Irtiza Ali',
       description="This module provides support to compare two lists that have same or different types of data in it.",
       long_description="This module provides support to compare two lists that have same or different types of data in it." +
                        "\n Examples \n" +
-                       "lis1 = [1, 2 ,3] \n" +
-                       "lis2 = [2, 1] \n" +
-                       "plc.compare_list(lis1, dlis2) # Frue \n" +
+                       "Open the ipython3 shell and import the modules\n" +
+                       "$ import listcompare as plc \n" +
 
-                       "lis1 = [1, 2, 3] \n" +
-                       "lis2 = [2, 1, '3'] \n" +
-                       "plc.compare_list(lis1, lis2) # False \n" +
+                       "$ lis1 = [1, 2 ,3] \n" +
+                       "$ lis2 = [2, 1] \n" +
+                       "$ plc.compare_list(lis1, dlis2) # Frue \n\n" +
 
-                       "lis1 = ['1', '2', '3'] \n" +
-                       "lis2 = ['2', '1'] \n" +
-                       "plc.compare_list(lis1, lis2) # False \n" +
+                       "$ lis1 = [1, 2, 3] \n" +
+                       "$ lis2 = [2, 1, '3'] \n" +
+                       "$ plc.compare_list(lis1, lis2) # False \n\n" +
 
-                       "lis1 = [1, 2, 3] \n" +
-                       "lis2 = [2, 1, 3] \n" +
-                       "plc.compare_list(lis1, lis2) # True \n" +
+                       "$ lis1 = ['1', '2', '3'] \n" +
+                       "$ lis2 = ['2', '1'] \n" +
+                       "$ plc.compare_list(lis1, lis2) # False \n\n" +
 
-                       "lis1 = [{1: 1, 2: 2}] \n" +
-                       "lis2 = [{1: 1, 2: 2}] \n" +
-                       "plc.compare_list(lis1, lis2) # True \n"
+                       "$ lis1 = [1, 2, 3] \n" +
+                       "$ lis2 = [2, 1, 3] \n" +
+                       "$ plc.compare_list(lis1, lis2) # True \n\n" +
 
-                       "lis1 = 123 \n" +
-                       "lis2 = [{1: 1, 2: 2}] \n" +
-                       "plc.compare_list(lis1, lis2) # Exception will be raised \n"
+                       "$ lis1 = [{1: 1, 2: 2}] \n" +
+                       "$ lis2 = [{1: 1, 2: 2}] \n" +
+                       "$ plc.compare_list(lis1, lis2) # True \n\n"
 
-                       "lis1 = [1, 'a', {1: 1, 2: 2}, 3] \n" +
-                       "lis2 = [3, 1, {1: 1, 2: 2}, 'a']] \n" +
-                       "plc.compare_list(lis1, lis2) # True \n",
+                       "$ lis1 = 123 \n" +
+                       "$ lis2 = [{1: 1, 2: 2}] \n" +
+                       "$ plc.compare_list(lis1, lis2) # Exception will be raised \n\n"
+
+                       "$ lis1 = [1, 'a', {1: 1, 2: 2}, 3] \n" +
+                       "$ lis2 = [3, 1, {1: 1, 2: 2}, 'a']] \n" +
+                       "$ plc.compare_list(lis1, lis2) # True \n\n",
 
       author_email='aliartiza75@yahoo.com',
       license='GNU GENERAL PUBLIC LICENSE',
@@ -43,11 +46,12 @@ setup(name='listcompare',
           'Topic :: Software Development :: Build Tools',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.0',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
       ],
-      keywords='list comparision python3 python2 python listofdifferentdatatypes',
+      keywords='list comparision python3 python2 python listofdifferentdatatypes pythonlists listcomparision',
       packages=['listcompare'],
       zip_safe=False)
